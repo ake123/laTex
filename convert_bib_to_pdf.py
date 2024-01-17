b9bfb3a544068f69e27dbba8876b7d24b8df51fb
ake123 <akeketema@yahoo.com>
2024-01-17 14:07:36 +0200

    some changes

---
+++ convert_bib_to_pdf.py
@@ -0,0 +1,29 @@
+import subprocess
+import pybtex.database.input.bibtex as bibtex_input
+
+# Define your .bib and .tex file names
+bib_file = "myreferences.bib"
+tex_file = "mydocument.tex"
+
+# Load the BibTeX file
+parser = bibtex_input.Parser()
+bib_data = parser.parse_file(bib_file)
+
+# Generate a LaTeX file with the bibliography
+with open(tex_file, "a") as tex_file:
+    tex_file.write("\n\n\\begin{document}\n\n")
+    for key in bib_data.entries.keys():
+        tex_file.write(f"\\cite{{{key}}}\n")
+    tex_file.write("\n\n\\bibliographystyle{plain}\n")
+    tex_file.write("\\bibliography{" + bib_file.split(".")[0] + "}\n")
+    tex_file.write("\n\n\\end{document}\n")
+
+# Compile the LaTeX document to generate the PDF
+try:
+    subprocess.run(["pdflatex", tex_file])
+    subprocess.run(["bibtex", tex_file.split(".")[0]])
+    subprocess.run(["pdflatex", tex_file])
+    subprocess.run(["pdflatex", tex_file])
+    print(f'Successfully generated {tex_file.split(".")[0]}.pdf')
+except Exception as e:
+    print(f"Error: {e}")
