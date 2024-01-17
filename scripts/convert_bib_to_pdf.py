6277ccdc873e52b0d75dba19edfd4a1c4b21c1bd
ake123 <akeketema@yahoo.com>
2024-01-17 15:37:44 +0200

    some changes

--- scripts/convert_bib_to_pdf.py
+++ scripts/convert_bib_to_pdf.py
@@ -2,35 +2,41 @@ import os
 import subprocess
 import pybtex.database.input.bibtex as bibtex_input
 
-# Get the directory where this script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Define the .bib and .tex file paths
+# Define your .bib and .tex file names
 bib_file = os.path.join(script_dir, "lahti.bib")
+tex_file = os.path.join(script_dir, "lahti.tex")
 
-# Specify the path to the .tex file one level up from the script folder
-tex_file = os.path.join(os.path.dirname(script_dir), "lahti.tex")
-
-# Define the output PDF file path as 'lahti.pdf' one level up from the script folder
-pdf_file_path = os.path.join(os.path.dirname(script_dir), "lahti.pdf")
+# Specify the output PDF file path
+pdf_file_path = os.path.join(script_dir, "lahti.pdf")
 
 # Load the BibTeX file
 parser = bibtex_input.Parser()
 bib_data = parser.parse_file(bib_file)
 
 # Generate a LaTeX file with the bibliography
-with open(tex_file, "a") as tex_file:
-    tex_file.write("\n\n\\begin{document}\n\n")
-    for key in bib_data.entries.keys():
-        tex_file.write(f"\\cite{{{key}}}\n")
-    tex_file.write("\n\n\\bibliographystyle{plain}\n")
-    tex_file.write("\\bibliography{" + bib_file.split(".")[0] + "}\n")
-    tex_file.write("\n\n\\end{document}\n")
+with open(tex_file, "w") as tex_file:
+    tex_file.write("\\documentclass[a4paper,12pt]{article}\n")
+    tex_file.write("\\usepackage[verbose,nomarginpar,a4paper,margin=25mm]{geometry}\n")
+    tex_file.write("\\usepackage[utf8]{inputenc}\n")
+    tex_file.write("\\usepackage[T1]{fontenc}\n")
+    tex_file.write("\\usepackage{lmodern}\n")
+    tex_file.write("\\usepackage{textcomp}\n")
+    tex_file.write("\\usepackage[datesep=.]{datetime2}\n")
+    tex_file.write(
+        "\\usepackage[backend=biber, style=alphabetic, maxnames=10, sorting=ydnt, texencoding=utf8]{biblatex}\n"
+    )
+    tex_file.write("\\addbibresource{" + bib_file + "}\n")
+    tex_file.write("\\begin{document}\n")
+    tex_file.write("\\nocite{*}\n")
+    tex_file.write("\\printbibliography\n")
+    tex_file.write("\\end{document}\n")
 
 # Compile the LaTeX document to generate the PDF
 try:
     subprocess.run(["pdflatex", tex_file])
-    subprocess.run(["bibtex", tex_file.split(".")[0]])
+    subprocess.run(["biber", tex_file.split(".")[0]])
     subprocess.run(["pdflatex", tex_file])
     subprocess.run(["pdflatex", tex_file])
 
